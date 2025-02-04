0d68726fd2dddaaa7625ea0eafe8933ac1e95174
Satyajeetkumarofficial <152394869+Satyajeetkumarofficial@users.noreply.github.com>
2025-02-04 09:35:05 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -90,7 +90,7 @@ async def start(client, message):
     if AUTH_CHANNEL and not await is_subscribed(client, message):
         try:
             if REQUEST_TO_JOIN_MODE == True:
-                invite_link = await client.create_chat_invite_link(int(AUTH_CHANNEL)), creates_join_request=True)
+                invite_link = await client.create_chat_invite_link(chat_id=(int(AUTH_CHANNEL)), creates_join_request=True)
             else:
                 invite_link = await client.create_chat_invite_link(int(AUTH_CHANNEL))
         except Exception as e:
